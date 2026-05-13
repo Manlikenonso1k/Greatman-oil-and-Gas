f564629a16fc2ee9986f762383b91d83a5624028
gitpushchrismith <chrismith1m@gmail.com>
2026-05-13 21:34:23 +0100

    Update WhatsApp contact to +2348032628137

--- config/services.php
+++ config/services.php
@@ -36,7 +36,7 @@ return [
     ],
 
     'whatsapp' => [
-        'url' => env('WHATSAPP_URL', 'https://www.whatsapp.com/'),
+        'url' => env('WHATSAPP_URL', 'https://wa.me/2348032628137'),
     ],
 
 ];
